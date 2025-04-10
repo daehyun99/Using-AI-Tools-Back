c0b60a5299d0f5e13c2e29828be1ba6819812723
daehyun99 <daehyun99@kakao.com>
2025-04-11 01:28:39 +0900

    Feat: Email 전송 샘플 코드 고도화 #56
    - background_task 적용

--- app/routes/EmailManager.py
+++ app/routes/EmailManager.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 
-from os import getenv
+from app.services.emailmanage import send_email_sample_
+from starlette.background import BackgroundTasks
 
-from app.api.response import SuccessResponse
 from app.common.utils import logging_response
 from app.common.utils import generate_metadata
 
+from app.api.response import SuccessResponse
 from app.api import exceptions as ex
 
 from sqlalchemy.orm import Session
@@ -16,66 +17,20 @@ router = APIRouter(prefix="/email")
 layer = "PRESENTATION"
 
 @router.post("/send/")
-async def email_send_sample(session: Session = Depends(db.get_db)):
+async def send_email_sample(background_tasks: BackgroundTasks, session: Session = Depends(db.get_db)):
     """
     `Email API`
     :return:
     """
     try:
-        # import
-        import smtplib
-        from email.mime.multipart import MIMEMultipart
-        from email.mime.text import MIMEText
-        from email.mime.application import MIMEApplication
-        from email.mime.image import MIMEImage
-
-        # 환경변수
-        test_receiver = getenv("EMAIL_RECEIVER")
-        sender = getenv("Email_SENDER")
-        smtp_server = getenv("SMTP_SERVER")
-        smtp_port = getenv("SMTP_PORT")
-        login_id = getenv("Email_LOGIN_ID")
-        login_pw = getenv("Email_LOGIN_PW")
-        
-        # 코드
         correlation_id = generate_metadata()
         # logging_request
-        subject = "PDF 파일 첨부 메일 전송 테스트"
-        receiver_name = test_receiver.split("@")[0]
-        body = f"""<html>
-            <head></head>
-            <body>
-                <h3 data-ke-size="size23"><b>안녕하세요!</b>&nbsp;{receiver_name}&nbsp;님. <br />PDF&nbsp;파일&nbsp;첨부&nbsp;<span style="background-color: #ee2323;">메일</span>&nbsp;<span style="color: #ee2323;">전송</span>&nbsp;<u>테스트</u>입니다. <br />✅🟥🗨✨😀 <br /><br />감사합니다. <br />{sender}&nbsp;드림.</h3>
-                <img src="cid:image1">
-            </body>
-        </html>
-        """
-        msg = MIMEMultipart("related")
-        msg["From"] = sender
-        msg["To"] = test_receiver
-        msg["Subject"] = subject
 
-        msg_alternative = MIMEMultipart("alternative")
-        msg.attach(msg_alternative)
-        msg_alternative.attach(MIMEText(body, "html"))
-
-        with open("tests/gooddog.jpg", "rb") as img:
-            mime_img = MIMEImage(img.read())
-            mime_img.add_header("Content-ID", "<image1>")
-            msg.attach(mime_img)
-
-        with open("tests/example.pdf", "rb") as f:
-            pdf_attachment = MIMEApplication(f.read(), _subtype="pdf")
-            pdf_attachment.add_header("Content-Disposition", "attachment", filename="테스트용-첨부파일.pdf")
-            msg.attach(pdf_attachment)
-
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(login_id, login_pw)
-            server.send_message(msg)
-
+        background_tasks.add_task(send_email_sample_, session=session, correlation_id=correlation_id)
+ 
         success_message = SuccessResponse()
         return logging_response(session=session, layer=layer, correlation_id=correlation_id, obj=success_message)
     except Exception as e:
         error_message = ex.ErrorResponse(ex=e)
         return logging_response(session=session, layer=layer, correlation_id=correlation_id, obj=error_message)
+    
