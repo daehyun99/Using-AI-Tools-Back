295e781a596299c0c8c363f4cb66e236dddb951e
daehyun99 <daehyun99@kakao.com>
2025-05-01 20:14:18 +0900

    Feat: 비밀번호 hash 적용 #59

--- app/routes/Auth.py
+++ app/routes/Auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from app.models import Email
 
+import bcrypt
 from app.routes.EmailManager import send_email_id_pw
 from app.common.utils import generate_id, generate_pw, generate_metadata, is_valid_email
 
@@ -43,9 +44,10 @@ async def register(email: Email, session: Session = Depends(db.get_db)):
         # 2. 이메일에 대한 고유 ID와 PW를 발급 및 DB로 저장    
             # ✅ 발급
             # ✅ DB 저장
-            # pw 해쉬 적용
+            # ✅ pw 해쉬 적용
         id, pw = generate_id(), generate_pw()
-        create_user(session=session, uuid=id, email=email.email, password_hash=pw, service_enabled=True)
+        password_hash = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
+        create_user(session=session, uuid=id, email=email.email, password_hash=password_hash, service_enabled=True)
         
         # 3. 해당 이메일로 고유 ID와 PW를 전송
         await send_email_id_pw(id, pw, email.email, session=session, correlation_id=correlation_id)
@@ -73,20 +75,23 @@ async def re_register(session: Session = Depends(db.get_db)):
             # 설문시트에서 고유 ID에 관한 정보 불러오기
             # 새로운 PW 발급 대상임을 검증 (설문 참여를 했는지)
             # 입력된 고유 ID에 관한, 이메일 주소 불러오기
-        
-        # email = load_email() # 설문시트에서 이메일 정보 로드 함수
-        email = test_receiver # 임시(이메일 정보 로드 함수)
+
+        # email = read_email() # 설문시트에서 이메일 정보 로드 함수
+
+        # user = read_user_by_email(session=session, email=email.email) # 해당 이메일의 고유 ID, PW 정보 불러오기
+        email = test_receiver # 임시
 
         # 2. 이메일에 대한 새로운 PW를 발급 및 DB 최신화
             # ✅ 발급
             # ✅ DB 수정
             # pw 해쉬 적용
         pw = generate_pw()
-        user = update_user(session=session, email=email, password_hash=pw, service_enabled=True)
+        password_hash = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
+        user = update_user(session=session, email=email, password_hash=password_hash, service_enabled=True)
 
 
         # 3. 해당 이메일로 갱신된 고유 ID와 PW를 전송
-        await send_email_id_pw(user.uuid, user.password_hash, user.email, session=session, correlation_id=correlation_id)
+        await send_email_id_pw(user.uuid, pw, user.email, session=session, correlation_id=correlation_id)
 
         # 4. 테스트 코드 작성
 
